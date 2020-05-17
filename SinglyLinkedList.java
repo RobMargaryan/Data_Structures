10040c7bf0145537c88ac4a8953c429c96e0ec71
Rob <pogrobert@me.com>
2020-05-17 16:37:13 +0400

    Added prev to Account.java Fixed errors in SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,45 +1,53 @@
-package Data_Structures;
-import Data_Structures.Account;
-
-
 public class SinglyLinkedList {
     private Account head = null;
     private Account tail = null;
     private int size = 0;
-    public SinglyLinkedList() { }
-         public int size( ) { return size; }
-    public boolean isEmpty( ) { return size == 0; }
-    public int first( ) {
-       if (isEmpty( )) return -1;
-        return head.getAccNum();
-        }
-    public int last( ) {
-        if (isEmpty( )) return -1;
-        return tail.getAccNum();
-        }
 
-        public void addFirst(String FirstName, String LastName, int AccNum, byte age) {
+    public SinglyLinkedList() {
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int first() {
+        if (isEmpty()) return -1;
+        return head.getID();
+    }
+
+    public int last() {
+        if (isEmpty()) return -1;
+        return tail.getID();
+    }
+
+    public void addFirst(String FirstName, String LastName, int AccNum, byte age) {
         head = new Account(FirstName, LastName, AccNum, age, head);
         if (size == 0)
             tail = head;
         size++;
-        }
+    }
+
     public void addLast(String FirstName, String LastName, int AccNum, byte age) {
-        Account newest = new Account(FirstName, LastName, AccNum, age,null);
-        if (isEmpty( ))
+        Account newest = new Account(FirstName, LastName, AccNum, age, null);
+        if (isEmpty())
             head = newest;
         else
-        tail.setNext(newest);
+            tail.setNext(newest);
         tail = newest;
         size++;
-        }
-    public int removeFirst( ) {
-        if (isEmpty( )) return -1;
-        int answer = head.getAccNum();
-        head = head.getNext( );
+    }
+
+    public int removeFirst() {
+        if (isEmpty()) return -1;
+        int answer = head.getID();
+        head = head.getNext();
         size--;
         if (size == 0)
             tail = null;
         return answer;
-        }
     }
+}
